301b9cc364c524a289135e7737c3b54514a4ab94
cheerlisten <cheerlisten@gmail.com>
2024-01-24 00:16:21 +0800

    c264.interface: add MemoryAllocator

--- src/c264.cpp
+++ src/c264.cpp
@@ -6,6 +6,45 @@
 #include <decoder.h>
 #include <log.h>
 
+struct MemoryAllocator_Default
+{
+    uint8_t* Alloca(uint32_t size)
+    {
+        return new uint8_t[size];
+    }
+    void DeAlloca(const uint8_t* buffer, uint32_t size)
+    {
+        delete[] buffer;
+    }
+    static MemoryAllocator* Instance()
+    {
+        static MemoryAllocator _ins = [] {
+            auto            fnAlloc = &MemoryAllocator_Default::Alloca;
+            auto            fnDeAlloca = &MemoryAllocator_Default::DeAlloca;
+            MemoryAllocator m;
+            m.alloc = *decltype(&m.alloc)(&fnAlloc);
+            m.dealloc = *decltype(&m.dealloc)(&fnDeAlloca);
+            m.opaque = nullptr;
+            return m;
+        }();
+        return &_ins;
+    }
+};
+
+extern "C" API uint8_t* AllocMemory(const uint32_t size, MemoryAllocator* allocator)
+{
+    if (!allocator)
+        allocator = MemoryAllocator_Default::Instance();
+    return allocator->alloc(allocator, size);
+}
+
+extern "C" API void FreeMemory(const uint8_t* buffer, int size, MemoryAllocator* allocator)
+{
+    if (!allocator)
+        allocator = MemoryAllocator_Default::Instance();
+    allocator->dealloc(allocator, buffer, size);
+}
+
 extern "C" API int CreateDecoder(IDecoder** ppDecoder)
 {
     if (ppDecoder == 0)
@@ -25,7 +64,8 @@ extern "C" API int DestroryDecoder(IDecoder** ppDecoder)
 
 void Log(const char* fmt, ...)
 {
-    char buf[1024];
+    char    buf[1024];
+    
     va_list ls;
     va_start(ls, fmt);
     int msgLen = vsnprintf(buf, 1024, fmt, ls);
--- src/inc/c264/c264.h
+++ src/inc/c264/c264.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <stdint.h>
 #include <c264/config.h>
-#include <cstdint>
 
 #define MAX_CHANNEL 3
 
-struct DecoderSetting
+struct MemoryAllocator
 {
+    uint8_t* (*alloc)(void* opaque, uint32_t size);
+    void (*dealloc)(void* opaque, const uint8_t* buf, uint32_t size);
+
+    /* in case of state-aware allocatator */
+    void* opaque;
 };
 
 struct BufferView
@@ -23,16 +28,26 @@ struct Picture
     uint32_t width, height;
 };
 
+struct DecoderSetting
+{
+};
+
 class IDecoder
 {
   public:
     virtual int Initialize(const DecoderSetting* setting) = 0;
     virtual int UnInitialize() = 0;
-    /// @return 0: OK,  -1: need retreive frames first, other: ... 
+    /// @return 0: OK,  -1: need retreive frames first, other: ...
     virtual int SendPacket(const BufferView* nalBufView) = 0;
-    /// @return 0: OK,  -1: need more input, other: ... 
+    /// @return 0: OK,  -1: need more input, other: ...
     virtual int GetFrame(const Picture** out_pic) = 0;
 };
+
+/// @brief Allocate a buffer with size=${size}, from allocator
+/// @param allocator leave zero for default allocator
+/// @return non-null:OK
+extern "C" API uint8_t* AllocMemory(const uint32_t size, MemoryAllocator* allocator = nullptr);
+extern "C" API void     FreeMemory(const uint8_t* buffer, int size, MemoryAllocator* allocator = nullptr);
 
 extern "C" API int CreateDecoder(IDecoder** ppDecoder);
 extern "C" API int DestroryDecoder(IDecoder** ppDecoder);
